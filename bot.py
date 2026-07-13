f372f373a5fa330fc3dbcc1a8f37d32b715df798
RineBinaurHW <ovcarenkopavel36@gmail.com>
2026-07-14 00:30:34 +0700

    bot.py
    скучаю по бывшей (ее не было)

--- bot.py
+++ bot.py
@@ -77,9 +77,12 @@ async def main():
 
     logger.info("Бот запущен и опрашивает сервер Telegram...")
 
-    async with application:  # <--- ОТСТУП (4 пробела)!
-        await application.start()
-        await application.updater.start_polling(allowed_updates=Update.ALL_TYPES)
+    async with application:
+    await application.start()
+    await application.updater.start_polling(allowed_updates=Update.ALL_TYPES)
+    # Бот будет работать, пока его не остановят. 
+    # Когда придёт сигнал остановки, он завершится без ошибки.
+
 
 if __name__ == "__main__":
     asyncio.run(main())
